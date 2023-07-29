791584c4086788ed2a49b9b4e6ae5a1ae027ef79
Dylan Kazunaga <dylankazunaga@Dylans-MacBook-Air.local>
2023-07-28 19:01:22 -0700

    updated typeDefs.js file

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -8,4 +8,14 @@ type User {
     bookCount: Int
     savedBooks: [Book]
 }
+
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+}
+
 `;
