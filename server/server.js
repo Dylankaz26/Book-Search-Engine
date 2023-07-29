d947bb3d2c47c462d189cd9ff4709f443fb92b73
Dylan Kazunaga <dylankazunaga@Dylans-MacBook-Air.local>
2023-07-28 18:53:09 -0700

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -21,8 +21,10 @@ const server = new ApolloServer({
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+});
 
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
